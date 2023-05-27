febb478334b932e20d573f8e9136b7432c2fe2e3
unknown <sniperalberto91@gmail.com>
2023-05-27 18:44:27 +0200

    App terminada, no contiene url a la api para que no se borre nada por accidente

--- app/src/main/java/com/example/f1api/settings/Constants.kt
+++ app/src/main/java/com/example/f1api/settings/Constants.kt
@@ -1,6 +1,6 @@
 package com.example.f1api.settings
 
-const val BASE_URL = "https://f1api-44edc-default-rtdb.europe-west1.firebasedatabase.app/"
+const val BASE_URL = ""
 const val DRIVER = "drivers.json"
 const val TEAM = "teams.json"
 const val CIRCUIT = "circuits.json"
