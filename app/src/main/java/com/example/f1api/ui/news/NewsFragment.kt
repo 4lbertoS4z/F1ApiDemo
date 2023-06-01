2e5c8316692d5ead2ca0bedd6f16813c878250ef
unknown <sniperalberto91@gmail.com>
2023-06-01 13:56:48 +0200

    correccion de un bug que encontre en el fragment webview

--- app/src/main/java/com/example/f1api/ui/news/NewsFragment.kt
+++ app/src/main/java/com/example/f1api/ui/news/NewsFragment.kt
@@ -21,7 +21,8 @@ class NewsFragment : Fragment() {
 
     // This property is only valid between onCreateView and
     // onDestroyView.
-    private val binding get() = _binding!!
+    private val binding get() = _binding ?: throw IllegalStateException("FragmentNewsBinding is not available.")
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
